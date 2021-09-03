0b553509bbdffb053447d9a318cc9508541058c5
Vaernion <vaporz@gmail.com>
2021-09-03 13:43:29 +0200

    handle outlier of file lengths

--- assignment02/traverse-hidden/find-flag.py
+++ assignment02/traverse-hidden/find-flag.py
@@ -4,12 +4,18 @@ from tqdm import tqdm
 INFIlE = "readmes-found.txt"
 MATCH = "flag"
 OUTFILE = "readmes-contents.txt"
+OUTFILE_LONG = "readme-contents-long.txt"
+USUAL_FILE_LENGTH = 69
 
-with open(OUTFILE, "w") as out_file:
-    num_lines = sum(1 for line in open(INFIlE, "r"))
-    with open(INFIlE, "r") as in_file:
-        for link in tqdm(in_file, total=num_lines):
-            content = requests.get(link.rstrip()).text
-            if MATCH in content.lower() or not content == content[0] * len(content):
-                print(content)
-            out_file.write(content + "\n")
+with open(OUTFILE_LONG, "w") as out_file_long:
+    with open(OUTFILE, "w") as out_file:
+        num_lines = sum(1 for line in open(INFIlE, "r"))
+        with open(INFIlE, "r") as in_file:
+            for link in tqdm(in_file, total=num_lines):
+                content = requests.get(link.rstrip()).text
+                if MATCH in content.lower() or not content == content[0] * len(content):
+                    # print(content)
+                    pass
+                out_file.write(content + "\n")
+                if len(content) != USUAL_FILE_LENGTH:
+                    out_file_long.write(f"LONG--{link.rstrip()}--{content}\n")
