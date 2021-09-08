3912e13dd710b764ece47ec63e8c78e33a701c4f
Vaernion <vaporz@gmail.com>
2021-09-08 02:19:18 +0200

    flag4 failed equation solver

--- assignment02/feedback-grade.py
+++ assignment02/feedback-grade.py
@@ -4,6 +4,8 @@
 # from wolframalpha asking next prime after 2^64
 NEXT_PRIME = 18446744073709551629
 
+flag3 = "24bea01a261043e2e5f504594e360ec5"
+flag3int = int(flag3, 16)  # unknown, needed to solve - convert hex to dec?
 
 # def is_prime(x):
 #     return all(x % i for i in range(2, x))
@@ -15,8 +17,21 @@ grade = 0  # unknown, goal to find
 grade_base = 2
 
 p = NEXT_PRIME  # Next_Prime(2^64)
-flag3 = 0
-flag3int = int(flag3, 16)  # unknown, needed to solve - convert hex to dec?
 right_side = flag3int % (2 ** 64)
 
-print(right_side)
+print(f"{right_side=} {p=}")
+
+
+left_side = 0
+n = 830_000  # update while progressing brute force
+PROGRESS_EVERY = 10_000
+i = 0
+
+while left_side != right_side:
+    if i % PROGRESS_EVERY == 0:
+        print(f"{i=} {n=} {left_side=}")
+    n += 1
+    i += 1
+    left_side = (2 ** n) % p
+
+print(f"{left_side=}  {n=}")
