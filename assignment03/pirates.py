44dc27ab14044fd9965c3be5906f7870fbeeb52d
Vaernion <vaporz@gmail.com>
2021-09-29 22:04:33 +0200

    rsa pirates cleaned up, removed unused mpmath

--- assignment03/pirates.py
+++ assignment03/pirates.py
@@ -4,50 +4,18 @@ import sys
 
 import Crypto
 import gmpy2
-import mpmath
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
-from gmpy2 import mpc, mpfr, mpq, mpz
-from mpmath import *
+from gmpy2 import mpz
 from sympy import *
 
-mp.prec = 100000
 gmpy2.get_context().precision = 100000
-# print(gmpy2.get_context())
-
-
-# def genKeys():
-#     # stuff
-
-#     pp = []
-#     qq = []
-#     for i in range(4):
-#         p = randprime(2 ** 1023, 2 ** 1024 - 1)
-#         pp.append(p)
-#         shift = randprime(2 ** 1045, 2 ** 1046 - 1)
-#         q = nextprime(p * p + shift)
-#         qq.append(q)
-
-#     nn = []
-#     for i in range(4):
-#         nn.append(pp[i] * qq[i])
-
-#     tmpRSAkeys = []
-#     for i in range(4):
-#         newd = Crypto.Util.number.inverse(65537, (pp[i] - 1) * (qq[i] - 1))
-#         newkey = RSA.construct((nn[i], 65537, newd, pp[i], qq[i]))
-#         tmpRSAkeys.append(newkey)
-
-#     # more stuff
 
 
 def breakRSA():
     public_keys = glob.glob(os.path.join(os.path.dirname(__file__), "keys/*"))
     solutions = []
     for index, key_filename in enumerate(public_keys):
-        # if index != 0:
-        #     continue
-
         public_key = RSA.importKey(open(key_filename).read())
         n = public_key.n  # int ~3017 bits
         e = public_key.e  # int 65537
@@ -70,15 +38,6 @@ def findKey(n, e, index, key_filename):
     n = mpz(n)
     USE_TEST = False
 
-    ### sanity check during debugging
-    # n_test = mpz(3233)
-    # p_test = mpz(61)
-    # q_test = n_test / p_test  # 53
-    # n2_test = q_test * p_test
-    # mod5 = n_test % p_test == 0
-    # print(n_test, n2_test, p_test, q_test, mod5)
-    # print(type(n_test), type(n2_test), n_test == n2_test, n_test % p_test == 0)
-
     #### SOLVED breaking randomly generated test value
     if USE_TEST:
         p_test = randprime(2 ** 1023, 2 ** 1024 - 1)
@@ -94,23 +53,11 @@ def findKey(n, e, index, key_filename):
 
     print(f"-- finding key {index=} {key_filename=}")
     P_MIN = mpz(nextprime(2 ** 1023 - 1))
-    # P_MAX = mpz(nextprime(2 ** 1024 - 1))
     SHIFT_MIN = mpz(nextprime(2 ** 1045 - 1))
-    # SHIFT_MAX = mpz(nextprime(2 ** 1046 - 1))
-    # print(f"{mpfr(P_MIN)=}\n{mpfr(P_MAX)=}")
 
-    # p_root = mpfr(n) ** (1 / 3)  # cube root
-    # n_iroot_rem = gmpy2.iroot_rem(n, 3)
-    # p_root = n_iroot_rem[0]
-    # p_root_remainder = n_iroot_rem[1]
-
-    # p_approx = p_root
-    # p_approx = ((n) - (P_MIN * SHIFT_MIN)) ** (1 / 3)
     p_approx = gmpy2.iroot_rem((n) - (P_MIN * SHIFT_MIN), 3)[0]
-    # p_approx = mpz(nextprime(p_approx))
     p = p_approx
-    q = mpz(1)
-    # print(f"{mpfr(p_approx)=}\n{p=}")
+    q = mpz(1)  # prevent UnboundLocalError during KeyboardInterrupt
 
     ERR_NOT_MATCH_MODULUS = "RSA factors do not match modulus"
     err_not_match_modulus_count = 0
@@ -121,7 +68,6 @@ def findKey(n, e, index, key_filename):
 
     while True:
         try:
-            # p = mpz(nextprime(p))
             p = mpz(prevprime(p))
             if USE_TEST:
                 if i % 100 == 0:
@@ -144,64 +90,32 @@ def findKey(n, e, index, key_filename):
                         f"### found p test, {(p==p_test)=} {(q==q_test)=} {(n2==n_test)=} {(rem==0)=} ###"
                     )
 
-            # q = mpz(n) / mpz(p)
             rem = gmpy2.t_mod(n, p)
-            # rem = n % p
             if rem != 0:
                 err_p_not_in_n += 1
                 continue
 
-            print(f"found P? {p=}")
+            # print(f"found P? {p=}")
             q = gmpy2.div(n, p)
             n2 = p * q
-            print(n2 == n, type(n2), type(n), type(p), type(q), mpfr.is_integer(q))
-            # while (n2) != n:
-            #     if n2 < n:
-            #         q = mpz(nextprime(q))
-            #     else:
-            #         q = mpz(prevprime(q))
-            #     n2 = p * q
-            #     print("diff:", n - (p * q))
-            #     # print(type(n), type(p), type(q))
-            # print("escaped loop")
-
-            # if not mpfr.is_integer(q):
-            #     raise Exception("Q division error")
-            # print(type(p), type(q))
-            # n2 = mpz(p * q)
-            # if n != n2:
-            #     err_n_not_equal_count += 1
-            #     # print(mpz(n), mpz(n2))
-            #     # print("\n\nn1:", n)
-            #     # print("n2", n2, type(int(n2)), type(n), "equal:", n == int(n2))
-            #     # print("n2", n2, "\n\n")
-            #     print("minus",n2 - n)
-            #     continue
+            # print(n2 == n, type(n2), type(n), type(p), type(q), mpfr.is_integer(q)) # verify valid values
 
             phi = mpz(p - 1) * mpz(q - 1)
-            # d = gmpy2.invert(mpz(e), phi)
             d = Crypto.Util.number.inverse(e, int(phi))
             rsa_components = (int(n), e, d, int(p), int(q))
-            # print(type(n), type(e), type(d), type(p), type(q))
-            # print([type(x) for x in rsa_components])
 
             newkey = RSA.construct(rsa_components)
             cipher_rsa = PKCS1_OAEP.new(newkey)
-            print("newkey", newkey)
-            print("cipher_rsa", cipher_rsa)
 
-            # readBins(cipher_rsa)
-            return cipher_rsa
+            return (key_filename, cipher_rsa)
 
         except KeyboardInterrupt:
             print(
                 f"\n\n{i=} {err_not_match_modulus_count=} {err_n_not_equal_count=} {err_not_implemented_count=} {err_p_not_in_n=}"
             )
-            # print(f"{p-p_approx =}")
             print(f"n-(p*q): {n-(p*q)}")
             sys.exit(1)
         except ValueError as err:
-            # print("findKey() ValueError:", err)
             if str(err) == ERR_NOT_MATCH_MODULUS:
                 err_not_match_modulus_count += 1
                 continue
@@ -215,28 +129,23 @@ def findKey(n, e, index, key_filename):
 
 
 def readBins(cipher_rsa):
-    print("readBins", cipher_rsa)
-
     binfiles = glob.glob(os.path.join(os.path.dirname(__file__), "bin/*"))
     for binfilename in binfiles:
         with open(binfilename, "rb") as file:
             try:
-                # print(binfilename)
                 encrypted_message = file.read()
+                message = cipher_rsa[1].decrypt(encrypted_message)
+                print(
+                    f"--DECRYPTED FILE: {binfilename} ENCODED WITH: {cipher_rsa[0]}\n{message=}\n\n"
+                )
 
-                # print(cipher_rsa)
-                message = cipher_rsa.decrypt(encrypted_message)
-                print(f"--FILE: {binfilename}\n{message=}\n\n")
-            except TypeError as err:
-                print(err)
-            except ValueError as err:
+            except (TypeError, ValueError) as err:
                 print(err)
                 continue
 
 
 def main():
     solutions = breakRSA()
-    print(f"{solutions=}")
     for solution in solutions:
         readBins(solution["cipher"])
 
